5d2e51f756a694137ce992c977634919c129f31d
Yahir Hernández <148612192+Ayayawo-Hdz@users.noreply.github.com>
2024-01-07 23:51:20 -0700

    https://codeforces.com/gym/103306/problem/D

---
+++ w4/disliketheraisins.cpp
@@ -0,0 +1,24 @@
+v#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long int
+#define ull unsigned long long int
+
+using namespace std;
+
+/*-------------------SOLBEGIN-------------------*/
+
+void solve() {
+    ll c, r, s; cin >> c >> r >> s;
+    cout << (((c+r)+s-1)/s) - ((r+s-1)/s) << " " << max(0ll, (((c+r)+s-1)/s) - r) << endl;
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
